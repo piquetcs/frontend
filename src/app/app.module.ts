41801fd1c66170d5d3a07d84fd4d0067789c7b3b
Travis W Richardson <trichardso022@nam.ad.pwcinternal.com>
2019-06-06 16:36:04 -0400

    Navigator added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import {ReactiveFormsModule, FormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent} from './login/login.component'
-import { LoginService} from './login/login.service'
+import { LoginService} from './login/login.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NavigatorComponent } from './navigator/navigator.component';
+import { ActivitiesListComponent } from './activities-list/activities-list.component';
+import { ActivityCardComponent } from './activity-card/activity-card.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    NavigatorComponent,
+    ActivitiesListComponent,
+    ActivityCardComponent
   ],
   imports: [
     BrowserModule,
--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,10 +1,15 @@
 import{Injectable} from '@angular/core';
+import {Router} from "@angular/router";
 
-Injectable()
+@Injectable()
 export class LoginService{
+
+    constructor(private router: Router){}
 
     login(userName: string, password: string){
         console.log(userName,password)
+
+        this.router.navigate(['dashboard/']);
     }
 
 }
